09742089fca2d282e3dad010b4a943ab1ef9baef
Rudresh Oza <ozarudresh98@gmail.com>
2022-07-01 08:05:51 +0530

    Day 9  - intro to JS

---
+++ DAY9/script.js
@@ -0,0 +1,45 @@
+// console.log("HI WORLD");
+// console.error("Hi this is a error.");
+// console.debug("Hi this is a debug");
+// console.warn("Hi this is warning.");
+// const obj = {
+//   name: "Nick",
+//   age: 10,
+//   gg: "jjjjj",
+// };
+// console.table(obj);
+// console.dir(obj);
+
+//  declaration of variable
+var userName = "Rudresh";
+var userName = "Nick";
+let userAge = 60;
+userAge = 70;
+const userDetails = "Web Developer";
+// console.log("username", userName);
+// console.table({ userAge });
+// console.table({ userDetails });
+
+let arr = [];
+let obj = {};
+let str = "";
+let num = 0;
+let bool = true;
+
+// function is JAVASCRIPT
+
+function multiply(params) {
+  console.log("multiply function", params);
+  return params * params;
+}
+
+const multiplyData = multiply(3);
+console.table({multiplyData});
+const add = (params) => {
+  console.log("add function");
+  return params + params;
+};
+const addData = add(10);
+console.table({ addData });
+
+// operators in JS
